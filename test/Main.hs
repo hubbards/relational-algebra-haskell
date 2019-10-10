4610bc51086aa182ee2ffde6dab891711549d393
Spencer Hubbard <hubbardspencerm@gmail.com>
2019-10-10 16:12:17 -0700

    Add filepath package

--- test/Main.hs
+++ test/Main.hs
@@ -1,8 +1,9 @@
+import System.FilePath ( (</>) )
 import Test.DocTest ( doctest )
 
 main :: IO ()
 main = doctest [
-    "src/Pair.hs"
-  , "src/Tuple.hs"
-  , "src/Relation.hs"
+    "src" </> "Pair.hs"
+  , "src" </> "Tuple.hs"
+  , "src" </> "Relation.hs"
   ]
